da1f96df3d31b84141b472fb156d1bccaef503b2
justcabyr <asulaiman@dervac.com>
2021-12-18 03:54:59 +0100

    Added error middleware

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,14 @@
+import HttpException from "../common/http-exception";
+import { Request, Response, NextFunction, response } from "express";
+import { request } from "http";
+
+export const errorHandler = (
+    error: HttpException,
+    request: Request,
+    response: Response,
+    next: NextFunction
+) => {
+    const status = error.statusCode || error.status || 500;
+
+    response.status(status).send(error);
+};
